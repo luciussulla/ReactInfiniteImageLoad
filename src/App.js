af5dd0dd8d8840007c2c465cbf52b666f82a2e90
luciussulla <maciek.kowalczyk@yandex.com>
2021-07-25 07:24:59 +0200

    style search box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
+import {FiSearch} from 'react-icons/fi'
+
 import './App.css';
+const key='ovLrqyczs4iuX0Y0VM0oCFbay4Hmk3_IeREbRu6Zq1k'
+const secKey = 'W0JsDQB2dxdvNkVxTVSzUwcM6dcRgYyDpZVyD31y_Ik'
+const url ='https://api.unsplash.com/'
+const photos = 'https://api.unsplash.com/photos'
+const search = 'https://api.unsplash.com/search/photos'
+const apiLink='https://api.unsplash.com/photos/?client_id=ovLrqyczs4iuX0Y0VM0oCFbay4Hmk3_IeREbRu6Zq1k'
+// import Photo from './components/Photo'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-wrapper">
+        <div className="search-wrapper">
+          <div className="search-glass"><FiSearch/></div>
+          <input className="search-input" type="text"/>
+        </div>
+      </div>
     </div>
   );
 }
